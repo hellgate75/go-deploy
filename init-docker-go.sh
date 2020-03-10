ff753ce0f9fad15ca8faf7dc2b1bc0a5c00aea28
hellgate75 <hellgate75@gmail.com>
2020-03-10 11:47:54 +0000

    improving travis file

--- init-docker-go.sh
+++ init-docker-go.sh
@@ -4,10 +4,11 @@ GITHUB_USER="hellgate75"
 PROJECT_NAME="go-deploy"
 BASE_FOLDER="$GOPATH/src/github.com/$GITHUB_USER"
 PROJECT_FOLDER="$BASE_FOLDER/$PROJECT_NAME"
-echo "Creating base folder $BASE_FOLDER into folder: GOPATH $GOPATH"
-mkdir -p $BASE_FOLDER
-echo "Linking project into folder: GOPATH $GOPATH"
-ln -s $PROJECT_FOLDER ./
+echo "Working dir: $(pwd)"
+echo "Creating base folder '$PROJECT_FOLDER' into folder: GOPATH '$GOPATH'"
+mkdir -p $PROJECT_FOLDER
+echo "Linking project folder: '$PROJECT_FOLDER' into folder: GOPATH '$GOPATH'"
+ln -s $(pwd) $PROJECT_FOLDER
 echo "Changing folder to $PROJECT_FOLDER"
 cd $PROJECT_FOLDER
 echo "Running go procedure into folder:$PROJECT_FOLDER"
