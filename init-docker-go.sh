3d7a5dda0b3a591df94a8cae7c57de15bd6e2105
hellgate75 <hellgate75@gmail.com>
2020-03-10 11:59:01 +0000

    improving travis file

--- init-docker-go.sh
+++ init-docker-go.sh
@@ -2,6 +2,8 @@
 export GO111MODULE=on
 GITHUB_USER="hellgate75"
 PROJECT_NAME="go-deploy"
+BUILD_MODE="exe"
+EXTENSION=""
 BASE_FOLDER="$GOPATH/src/github.com/$GITHUB_USER"
 PROJECT_FOLDER="$BASE_FOLDER/$PROJECT_NAME"
 echo "Working dir: $(pwd)"
@@ -14,5 +16,8 @@ cd $PROJECT_FOLDER
 echo "Running go procedure into folder:$PROJECT_FOLDER"
 go mod init
 go mod tidy
-go test -v ./...
-go build -v ./...
+echo "Testing project into folder:$PROJECT_FOLDER"
+go test -v .
+OUT_FILE_NAME="$PROJECT_NAME-$(uname -o)-$(uname -m)$EXTENSION"
+echo "Building project for making: $OUT_FILE_NAME"
+go build -v -buildmode "$BUILD_MODE" -o "$OUT_FILE_NAME"
